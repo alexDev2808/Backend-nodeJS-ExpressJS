f25f1887c6a4609ae1cbe547a9e3dd57438083c9
AlexDev2808 <colores.2808.8000@gmail.com>
2023-01-08 17:16:59 -0600

    Comenzando a utilizar el PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -54,6 +54,24 @@ router.post('/', (req, res) => {
   })
 })
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "update",
+    data: body,
+    id
+
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Deleted",
+    id
+  })
+})
 
 // Se exporta como un modulo y tiene su propio routing
 
