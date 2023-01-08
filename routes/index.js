a3f865bdb77755af8e74ebb0c42c8917e01fb1b6
AlexDev2808 <colores.2808.8000@gmail.com>
2023-01-08 15:27:37 -0600

    Separacion de responsabilidades con Express Router

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+
+//
+
+const productsRouter = require('./products.router')
+const categoriesRouter = require('./categories.router')
+const usersRouter = require('./users.router.js')
+
+
+function routerApi(app){
+  app.use('/api/products', productsRouter);
+  app.use('/api/users', usersRouter);
+  app.use('/api/categories', categoriesRouter)
+}
+
+module.exports = routerApi
---
+++ routes/products.router.js
@@ -0,0 +1,50 @@
+
+// Generar un router especifico para los productos
+
+const express = require('express')
+const faker = require('faker')
+
+
+const router = express.Router();
+
+// Aqui no se deja el entpoint con productos sino solo los detalles, lo que iria despues del /
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+
+  }
+  res.json(products)
+})
+
+// En esta posicion si se esta tomando el endpoint especifico como se espera
+router.get('/filter', (req, res) => {
+  res.send("Yo soy un filter")
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+      id,
+      name: 'Producto 2',
+      price: 120
+  })
+})
+
+// Filter se esta tomando como parametro, para solucionarlo simplemente se debe colocaar antes del endpoint dinamico
+
+// app.get('/products/filter', (req, res) => {
+//   res.send("Yo soy un filter")
+// })
+
+
+// Se exporta como un modulo y tiene su propio routing
+
+module.exports = router
